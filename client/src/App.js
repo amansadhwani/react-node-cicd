ed6379cb08b048f2d6b48408e4b4c00c701debef
amansadhwani <amansadhwani@gmail.com>
2022-05-28 17:20:26 +0530

    removed code for node

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
-import './App.css';
-import React,{useEffect, useState} from 'react';
-import axios from 'axios'
+import "./App.css";
+import React, {  useState } from "react";
 function App() {
-  const [name,setName]=useState('');
-  const getName = async () =>{
-    const response = await axios.get("/name");
-    setName(response.data)
-  }
-  useEffect(()=>{
-    getName()
-  },[])
+  const name = "Aman";
+  const [data, setData] = useState([
+    { name: "Rafa", id: 0 },
+    { name: "Nadal", id: 1 },
+    { name: "Roger", id: 2 },
+    { name: "Federer", id: 3 },
+  ]);
+
+  const onDelete = (key) => {
+    const filterData = data.filter((item) => {
+      return item.id !== key;
+    });
+    setData(filterData);
+  };
+
   return (
     <div className="App">
-     <h1>Hello Welcome to CICD Pipeline</h1> 
-     <h3>My Name is {name}</h3> 
+      <h1>Hello Welcome to CICD Pipeline</h1>
+      <h3>My Name is {name}</h3>
+      {data.map((item) => {
+        return (
+          <h4 onClick={() => onDelete(item.id)} key={item.id}>
+            {item.name}
+          </h4>
+        );
+      })}
     </div>
   );
 }
